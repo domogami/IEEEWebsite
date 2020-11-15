7add4493d0991bd73e70edb3509d934ebc65474c
Dominick <d3lee@ucsd.edu>
2020-11-15 14:49:20 -0800

    ❤️ Sponsor Page finally done :))

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -22,30 +22,35 @@ const SponsorPage = () => (
   
   <div>
     <div className="sidebar">
-    <Sidebar />
-   </div>
-   <div className="SponsorWrapper">
-    <div className="SponsorMain">
-      <div className="SponsorPageText">
-        <h1>Hello</h1>
-      <p1>IEEE Student Branch at UC San Diego is a non-profit organization focused on developing bright engineers to serve the community around us by hosting various events, community outreach, and several technical project national competitions. These programs would not have been possible without the generousity of our sponsors. Thus, we invite you to support and be a part of our efforts to strengthen our future scientists and engineers.</p1>
-      <button className="learn-more">
-        <span
-          className="circle"
-          aria-hidden="true"
-          style={{ background: mainColors.Yellow }}
-        >
-          <span className="icon arrow"></span>
-        </span>
-        <a href={SponsorBrochure} download="Sponsor" className="button-text">
-          Sponsor Info
-        </a>
-      </button>
-      <p1>Contact our current Vice-Chair External, Lavita Zuo, at ieee@eng.ucsd.edu or x5zuo@ucsd.edu for more information!</p1>
-      <h1>A Big Thank You to Our Current Sponsors</h1>
-      <img src={Sponsors} className="SponsorImage" ></img>
-      </div>
+      <Sidebar />
     </div>
+    <div className="SponsorWrapper">
+      <div className="SponsorMain">
+        <div className="SponsorPageText">
+          <h1>Become a Sponsor</h1>
+          <div className="SponsorPageInfo">
+            <p1>IEEE Student Branch at UC San Diego is a non-profit organization focused on developing bright engineers to serve the community around us by hosting various events, community outreach, and several technical project national competitions. These programs would not have been possible without the generousity of our sponsors. Thus, we invite you to support and be a part of our efforts to strengthen our future scientists and engineers.</p1>
+            
+            <button className="learn-more">
+              <span
+                className="circle"
+                aria-hidden="true"
+                style={{ background: mainColors.Yellow }}
+              >
+                <span className="icon arrow"></span>
+              </span>
+              <a href={SponsorBrochure} download="Sponsor" className="button-text">
+                Sponsor Info
+              </a>
+            </button>
+            <p2>Contact our current Vice-Chair External, Lavita Zuo, at <a href="mailto:ieee@eng.ucsd.edu">ieee@eng.ucsd.edu</a> or <a href="mailto:x5zuo@ucsd.edu">x5zuo@ucsd.edu</a> for more information!</p2>
+          </div>
+        </div>
+        <div className="ThankYouWrapper">
+          <h1>A Big Thank You to Our Current Sponsors</h1>
+          <img src={Sponsors} className="SponsorImage" ></img>
+        </div>
+      </div>
     </div>
   </div>
 );
